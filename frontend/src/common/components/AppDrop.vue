7cf22e01a4c51075b3c162f965c5937818ee11b3
Maxim Sergeev <MaximGrekk@gmail.com>
2023-07-20 23:30:11 +0300

    Added AppDrag and AppDrop components for drag & drop interaction in the application.

---
+++ frontend/src/common/components/AppDrop.vue
@@ -0,0 +1,27 @@
+<template>
+  <div draggable @drop.stop="onDrop" @dragover.prevent @dragenter.prevent>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { DATA_TRANSFER_PAYLOAD } from "../constants";
+
+const emit = defineEmits(["drop"]);
+
+function onDrop({ dataTransfer }) {
+  if (!dataTransfer) {
+    return;
+  }
+  const payload = dataTransfer.getData(DATA_TRANSFER_PAYLOAD);
+
+  if (payload) {
+    const transferData = JSON.parse(
+      dataTransfer.getData(DATA_TRANSFER_PAYLOAD)
+    );
+    emit("drop", transferData);
+  }
+}
+</script>
+
+<style scoped></style>
